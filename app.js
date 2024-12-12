37da02ece764743f81a8d6f75302edc6f9c9dbd2
Jaewon <wellbird@naver.com>
2024-12-12 22:16:01 +0900

    FEAT : app.js 생성

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require(dotenv);
+dotenv.config();
+
+app.use(express.json());
+
+app.listen(process.env.PORT);
+
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const likeRouter = require('./routes/likes');
+const orderRouter = require('./routes/orders');
+const cartRouter = require('./routes/carts');
+
+app.use('/users', userRouter);
+app.use('/books', bookRouter);
+app.use('/likes', likeRouter);
+app.use('/orders', orderRouter);
+app.use('/carts', cartRouter);
