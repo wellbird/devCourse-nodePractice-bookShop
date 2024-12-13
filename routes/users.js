f018add9ec43a187ff40db6c75315aec9e617d54
Jaewon <wellbird@naver.com>
2024-12-13 15:57:45 +0900

    FEAT : user 컨트롤러와 라우터 연결결

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { signup, signin, passwordResetRequest, passwordReset } = require('../controllers/userController');
 
 // 회원가입
-router.post('/signup', (req, res) => {});
+router.post('/signup', signup);
 
 // 로그인
-router.post('/signin');
+router.post('/signin', signin);
 
 // 비밀번호 초기화
-router.post('/reset');
-router.put('/reset');
+router.post('/reset', passwordResetRequest);
+router.put('/reset', passwordReset);
 
 module.exports = router;
