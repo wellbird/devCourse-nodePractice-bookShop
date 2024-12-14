07a547fd3cb33868f004cb72054581d78ba32e46
Jaewon <wellbird@naver.com>
2024-12-14 15:32:53 +0900

    FEAT : 카테고리 라우터 추가가

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.listen(process.env.PORT);
 
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/categories');
 const likeRouter = require('./routes/likes');
 const orderRouter = require('./routes/orders');
 const cartRouter = require('./routes/carts');
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
+app.use('/category', categoryRouter);
 app.use('/likes', likeRouter);
 app.use('/orders', orderRouter);
 app.use('/carts', cartRouter);
