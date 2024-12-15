fbfb9a7adf3eb9865c296e45a21ed5b977c83d7c
Jaewon <wellbird@naver.com>
2024-12-15 21:40:26 +0900

    FEAT : 신간 조회, 페이징 쿼리문 구현

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,27 +2,33 @@ const conn = require('../mairadb');
 const { StatusCodes } = require('http-status-codes');
 
 const allBooks = (req, res) => {
-  let { categoryId } = req.query;
-  if (categoryId) {
-    let sql = 'SELECT * FROM books WHERE category_id = ?';
-    conn.query(sql, categoryId, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    let sql = 'SELECT * FROM books';
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  let { categoryId, newBook, limit, curPage } = req.query;
+
+  let sql = 'SELECT * FROM books';
+  let offset = limit * (curPage - 1);
+  let values = [];
+
+  if (categoryId && newBook) {
+    sql += ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+    values.push(categoryId);
+  } else if (categoryId) {
+    sql += ' WHERE category_id = ?';
+    values.push(categoryId);
+  } else if (newBook) {
+    sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
   }
+
+  sql += ' LIMIT ? OFFSET ?';
+  values.push(parseInt(limit), offset);
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 const bookDetail = (req, res) => {
