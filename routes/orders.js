a66208de0509c44bf42f8f81368c0e15096d89f6
Jaewon <wellbird@naver.com>
2025-02-09 21:35:37 +0900

    fix : 요청 메소드 수정

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { order, getOrders, getOrderDetail } = require('../controllers/orderController');
+const {
+  order,
+  getOrders,
+  getOrderDetail,
+} = require('../controllers/orderController');
 const router = express.Router();
 
 // 주문 하기
@@ -9,6 +13,6 @@ router.post('/', order);
 router.get('/', getOrders);
 
 // 주문 상세 상품 조회
-router.delete('/:id', getOrderDetail);
+router.get('/:id', getOrderDetail);
 
 module.exports = router;
